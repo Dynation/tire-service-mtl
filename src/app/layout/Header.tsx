efcd968c342c8ff623252120758b6668cf4c1651
dynation <dynazenklova@protonmail.com>
2024-12-02 00:36:26 -0500

    router header

--- src/app/layout/Header.tsx
+++ src/app/layout/Header.tsx
@@ -2,7 +2,7 @@
 
 "use client";
 import React, { useEffect, useState } from "react";
-import { useRouter } from "next/navigation";
+//import { useRouter } from "next/navigation";
 import {
   onAuthStateChanged,
   signInWithPopup,
@@ -17,7 +17,7 @@ import WheelIcon from "../../../public/images/wheel.svg";
 
 const Header: React.FC = () => {
   const [user, setUser] = useState<User | null>(null);
-  const router = useRouter();
+  //const router = useRouter();
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
